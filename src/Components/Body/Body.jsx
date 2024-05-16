5f7d0013de0c20be631d51882b1f33d61ba79c34
mutantbuhanov <101910958+mutantbuhanov@users.noreply.github.com>
2024-05-16 05:56:59 +0500

    content

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,15 +1,129 @@
 import React from 'react'
 import './body.css'
 import Carrousel from './CarouselPage.jsx'
+import bkg from './images/tweed.png'
 
 const Body = () => {
   return (
     <div>
         <div className="bodyy">
 <div className="carsl">
-
 <Carrousel/>
+</div>
+<div className="info1">
+  <div className="textrow1">
+<div className="t1">
+<p id='tt1'>WE HELP YOU</p>
+<p id='tt2'>ONE SIMPLE FEE</p>
+<p id='tt3'>¥500</p>
+<p id='tt4'>PER ITEM.
+<span>
+  <br />
+<a href="#" id='ahr'>WHATS INCLUDED?</a>
+   </span>
+</p>
+</div>
+  </div>
+<div className="textrow2">
+<p id='tb1'>BUY</p>
 
+<p id='tb2'>CREDIT CARD
+<br />
+<span>DEBIT CARD
+<br />
+<span>PAYPAL, CRYPTOCURRENCY
+  <br />
+  <span>
+    BANK TRANSFER
+  </span>
+</span>
+</span>
+</p>
+</div>
+<div className="textrow3">
+<p id='tc1'>STUFF FROM JAPAN</p>
+
+<p id='tc2'>RAKUTEN 
+<br />
+<span>
+  <p>
+AMAZON 
+<br />
+<span>
+  <p>
+    YAHOO 
+    <br />
+    <span>
+  <p>
+    ANY OTHER ONLINE MARKETPLACE
+  </p>
+</span>
+  </p>
+</span>
+  </p>
+</span>
+</p>
+</div>
+
+<div className="textrow4">
+  <p id='td1'>AND SEND IT</p>
+
+  <p id='td2'>
+    EMS 
+    <br />
+    <span>
+      <p>
+        AIRMAIL 
+        <br />
+    <span>
+      <p>
+        FEDEX 
+        <br />
+    <span>
+      <p>
+        DHL, UPS
+        <br />
+    <span>
+      <p>
+        ZENEXPRESS
+      </p>
+    </span>
+      </p>
+    </span>
+      </p>
+    </span>
+      </p>
+    </span>
+  </p>
+</div>
+<div className="textrow5">
+<p id='te1'>DIRECTLY TO YOU</p>
+
+<p id='te2'>USA 
+<br />
+<span>
+  <p>
+    EUROPE 
+    <br />
+<span>
+  <p>
+    ASIA 
+    <br />
+<span>
+  <p>
+    ALL OTHER COUNTRIES 
+  </p>
+</span>
+  </p>
+</span>
+  </p>
+</span>
+
+</p>
+
+
+
+</div>
 </div>
 
         </div>
